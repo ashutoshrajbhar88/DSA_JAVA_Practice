7657d5f61e802830ae84223831c5e711e77b99eb
ASHUTOSH RAJBHAR <153089570+ashutoshrajbhar88@users.noreply.github.com>
2026-03-20 22:47:35 +0530

    Time: 8 ms (54.4%), Space: 44.3 MB (40.69%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,28 +1,24 @@
-public class Solution {
+class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int len1 = s1.length(), len2 = s2.length();
-        if (len1 > len2) return false;
-        
-        int[] count = new int[26];
-        for (int i = 0; i < len1; i++) {
-            count[s1.charAt(i) - 'a']++;
-            count[s2.charAt(i) - 'a']--;
+        if (s1.length() > s2.length() || s2.length() == 0) return false;
+        if (s1.length() == 0) return true;
+
+        int x = s1.length(), y = s2.length();
+        int[] array1 = new int[26];
+        int[] array2 = new int[26];
+
+        for (int i = 0; i < x; i++) {
+            array1[s1.charAt(i) - 'a']++;
+            array2[s2.charAt(i) - 'a']++;
         }
-        if (allZero(count)) return true;
-        
-        for (int i = len1; i < len2; i++) {
-            count[s2.charAt(i) - 'a']--;
-            count[s2.charAt(i - len1) - 'a']++;
-            if (allZero(count)) return true;
+
+        for (int i = x; i < y; i++) {
+            if (Arrays.equals(array1, array2)) return true;
+
+            array2[s2.charAt(i - x) - 'a']--; // remove old
+            array2[s2.charAt(i) - 'a']++;     // add new
         }
-        
-        return false;
-    }
-    
-    private boolean allZero(int[] count) {
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) return false;
-        }
-        return true;
+
+        return Arrays.equals(array1, array2);
     }
 }
