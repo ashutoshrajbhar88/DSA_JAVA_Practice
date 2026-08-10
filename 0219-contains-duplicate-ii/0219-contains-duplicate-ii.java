7f40a1f73c5f6e38fdbb8db7a24e2c50a3b2877a
ASHUTOSH RAJBHAR <153089570+ashutoshrajbhar88@users.noreply.github.com>
2026-08-11 01:31:14 +0530

    Time: 28 ms (38.82%), Space: 111.4 MB (12.46%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,15 +1,41 @@
+// class Solution {
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+//         HashMap<Integer, Integer> map = new HashMap<>();
+
+//         for(int i =0; i<nums.length;i++){
+//             if(map.containsKey(nums[i])){
+//                 int prev = map.get(nums[i]);
+//             }
+//             if(i - prev <= k ){
+//                 return true;
+//             }
+//         }
+//         map.put(nums[i],i);
+        
+//     }
+//     return false;
+// }
+
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Map<Integer, Integer> seen = new HashMap<>();
+
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            int val = nums[i];
-            if (seen.containsKey(val) && i - seen.get(val) <= k) {
-                return true;
+
+            if (map.containsKey(nums[i])) {
+
+                int prev = map.get(nums[i]);
+
+                if (i - prev <= k) {
+                    return true;
+                }
             }
-            seen.put(val, i);
+
+            map.put(nums[i], i);
         }
 
-        return false;        
+        return false;
     }
 }
