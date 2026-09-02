512428691f55c88f1c354eda13bf97ae31577083
ASHUTOSH RAJBHAR <153089570+ashutoshrajbhar88@users.noreply.github.com>
2026-09-02 17:24:16 +0530

    Time: 19 ms (83%), Space: 45.6 MB (88.43%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -2,47 +2,57 @@ class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
 
         List<List<Integer>> result = new ArrayList<>();
+
         Arrays.sort(nums);
+        int n = nums.length;
 
-        HashSet<String> unique = new HashSet<>();
+        for (int i = 0; i < n - 3; i++) {
 
-        for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
 
-            for (int j = i + 1; j < nums.length - 2; j++) { 
+            for (int j = i + 1; j < n - 2; j++) {
+
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
 
                 int left = j + 1;
-                int right = nums.length - 1;
+                int right = n - 1;
 
                 while (left < right) {
 
-                    long sum = 0;
-                    sum += nums[i];
-                    sum += nums[j];
-                    sum += nums[left];
-                    sum += nums[right];
+                    long sum = (long) nums[i]
+                            + nums[j]
+                            + nums[left]
+                            + nums[right];
 
-                    if (sum < target) {
+                    if (sum == target) {
+
+                        result.add(Arrays.asList(
+                                nums[i],
+                                nums[j],
+                                nums[left],
+                                nums[right]
+                        ));
+
                         left++;
-                    } 
-                    else if (sum > target) {
                         right--;
-                    } 
-                    else {
 
-                        StringBuilder sb = new StringBuilder();
-                        sb.append(nums[i]).append(",");
-                        sb.append(nums[j]).append(",");
-                        sb.append(nums[left]).append(",");
-                        sb.append(nums[right]);
-
-                        String code = sb.toString();
-
-                        if (!unique.contains(code)) {
-                            unique.add(code);
-                            result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while (left < right &&
+                                nums[left] == nums[left - 1]) {
+                            left++;
                         }
 
+                        while (left < right &&
+                                nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+
+                    } else if (sum < target) {
                         left++;
+                    } else {
                         right--;
                     }
                 }
