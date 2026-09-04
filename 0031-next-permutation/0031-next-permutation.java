6a6b80fdf6e9b86ca4fdb9f6cd9968f71672b3e6
ASHUTOSH RAJBHAR <153089570+ashutoshrajbhar88@users.noreply.github.com>
2026-09-04 08:30:55 +0530

    Time: 0 ms (100%), Space: 44.7 MB (33.36%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -3,11 +3,16 @@ class Solution {
         int n = nums.length;
         int i = n - 2;
 
-        while (i >= 0 && nums[i] >= nums[i + 1]) i--;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
 
         if (i >= 0) {
             int j = n - 1;
-            while (nums[j] <= nums[i]) j--;
+
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
 
             int temp = nums[i];
             nums[i] = nums[j];
