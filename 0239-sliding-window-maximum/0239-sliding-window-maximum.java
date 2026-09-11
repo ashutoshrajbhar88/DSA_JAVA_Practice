c29bd603e464324c387a09fadbb9b76aec628c03
ASHUTOSH RAJBHAR <153089570+ashutoshrajbhar88@users.noreply.github.com>
2026-09-11 09:51:12 +0530

    Time: 27 ms (94.8%), Space: 146.7 MB (48.62%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,33 +1,21 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-
-        Deque<Integer> dq = new LinkedList<>();
-        int n = nums.length;
-
-        int[] result = new int[n - k + 1];
-        int idx = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            // Remove indices outside current window
-            while (!dq.isEmpty() && dq.peekFirst() <= i - k) {
-                dq.pollFirst();
+        int [] res= new int[nums.length - k +1];
+        int j = 0;
+        ArrayDeque<Integer> queue = new ArrayDeque();
+        for(int i = 0;i<nums.length;i++){
+            while(!queue.isEmpty() && nums[queue.getLast()] < nums[i]){
+                queue.removeLast();
             }
-
-            // Remove smaller elements from back
-            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
-                dq.pollLast();
+            queue.addLast(i);
+            if(queue.getFirst() +k==i){
+                queue.removeFirst();
             }
-
-            // Add current index
-            dq.offerLast(i);
-
-            // Store answer when first window is complete
-            if (i >= k - 1) {
-                result[idx++] = nums[dq.peekFirst()];
+            if(i >= k -1){
+                res[j++] = nums[queue.getFirst()];
             }
         }
-
-        return result;
+        return res;
+        
     }
 }
